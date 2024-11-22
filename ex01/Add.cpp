1b2c8343f652d51340727ec854115b1f01251b29
ychagri <ychagri@student.1337.ma>
2024-11-22 22:37:53 +0100

    ex02 done except for the distructor being called in reverse

--- ex01/Add.cpp
+++ ex01/Add.cpp
@@ -2,18 +2,22 @@
 
 bool IsWhiteSpace(const std::string& str)
 {
+
     for(size_t i = 0; i < str.length(); i++)
         if (!std::isspace(str[i]))
             return (false);
+
     return (true);
 }
 
 
 bool IsAllDigit(const std::string& str)
 {
+
     for(size_t i = 0; i < str.length(); i++)
         if (!std::isdigit(str[i]))
             return (false);
+
     return (true);
 }
 
@@ -25,11 +29,13 @@ void    DisplayFill(Contact& contact,const std::string msg, int flag)
     {
         std::cout << msg;
         std::getline(std::cin, GetInfo);
+
         if (GetInfo.empty() || IsWhiteSpace(GetInfo))
         {
             std::cerr << RED"\n*This can't be an empty field !!*" RESET << std::endl;
             continue;
         }
+
         else
         {
 			if (flag == NUM && IsAllDigit(GetInfo) == false)
@@ -37,6 +43,7 @@ void    DisplayFill(Contact& contact,const std::string msg, int flag)
             	std::cerr << RED"\n*Phone number should only contain digits!!*" RESET << std::endl;
 				continue ;
 			}
+
             contact.SetContactField(GetInfo, flag);
             return ;
         }
---
+++ ex02/Account.cpp
@@ -0,0 +1,114 @@
+#include "Account.hpp"
+#include <iostream>
+#include <ctime>
+
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+
+int	Account::getNbAccounts( void )
+{
+	return _nbAccounts;
+}
+
+int	Account::getTotalAmount( void )
+{
+	return _totalAmount;
+}
+
+int	Account::getNbDeposits( void )
+{
+	return _totalNbDeposits;
+}
+
+int	Account::getNbWithdrawals( void )
+{
+	return _totalNbWithdrawals;
+}
+
+int	Account::checkAmount( void ) const
+{
+	return _amount;
+}
+
+
+void	Account::_displayTimestamp( void )
+{
+	const time_t	now = time(0);
+	struct tm		*tstruct = localtime(&now);
+	char			timestr[20];
+	strftime(timestr, sizeof(timestr), "%Y%m%d_%H%M%S", tstruct);
+	std::cout << "[" << timestr << "]";
+}
+
+
+Account::Account(int initial_deposit)
+{
+	_accountIndex = _nbAccounts;
+	_nbDeposits = 0;
+	_nbWithdrawals = 0;
+	_amount = initial_deposit;
+	_totalAmount += checkAmount();
+	_displayTimestamp();
+	std::cout << " index:" << _accountIndex << ";amount:" << checkAmount() << ";created" << std::endl;
+	_nbAccounts++;
+}
+
+
+void	Account::displayAccountsInfos( void )
+{
+	_displayTimestamp();
+	std::cout << " account:" << getNbAccounts() << ";total:" << getTotalAmount()
+		<< ";deposits:" << getNbDeposits() << ";withdrawals:" << getNbWithdrawals() << std::endl;
+}
+
+
+void	Account::makeDeposit( int deposit )
+{
+	_displayTimestamp();
+	std::cout << " index:" << _accountIndex << ";p_amount:" << checkAmount() << ";deposit:" << deposit;
+	_amount += deposit;
+	_totalAmount += deposit;
+	_totalNbDeposits++;
+	_nbDeposits++;
+	std::cout <<  ";amount:" << checkAmount() << ";nb_deposits:" << _nbDeposits << std::endl;
+}
+
+
+bool	Account::makeWithdrawal( int withdrawal )
+{
+	_displayTimestamp();
+	std::cout << " index:" << _accountIndex << ";p_amount:" << checkAmount() << ";withdrawal:" ;
+	if (withdrawal > getTotalAmount() || withdrawal > checkAmount())
+	{
+		std::cout << "refused" << std::endl;
+		return false ;
+	}
+	_nbWithdrawals++;
+	_amount -= withdrawal;
+	_totalAmount -= withdrawal;
+	_totalNbWithdrawals++;
+	std::cout << withdrawal << ";amount:" << checkAmount() << ";nb_withdrawals:" << _nbWithdrawals << std::endl;
+	return true;
+}
+
+
+void	Account::displayStatus( void ) const
+{
+	_displayTimestamp();
+	std::cout << " index:" << _accountIndex << ";amount:" << checkAmount() << ";deposits:" << _nbDeposits
+		<< ";withdrawals:" << _nbWithdrawals << std::endl;
+}
+
+
+Account::~Account( void )
+{
+	_displayTimestamp();
+	std::cout << " index:" << _accountIndex << ";amount:" << checkAmount() << ";closed" << std::endl;
+	// _nbAccounts--;
+	// _totalAmount -= checkAmount();
+	// _amount = 0;
+}
