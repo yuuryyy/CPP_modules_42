f37e375a9689b9a5f9bc6b62de7581b6bdd3cafb
ychagri <ychagri@student.1337.ma>
2024-11-23 17:17:27 +0100

    cpp01 ex02 done

---
+++ CPP01/ex02/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main()
+{
+	std::string	StrTest("HI THIS IS BRAIN");
+	std::string* stringPTR = &StrTest;
+	std::string& stringREF = StrTest;
+
+	std::cout << "The memory address of the string variable :[";
+	std::cout << &StrTest <<  "]." << std::endl;
+
+	std::cout << "The memory address held by stringPTR      :[";
+	std::cout << stringPTR << "]." << std::endl;
+
+	std::cout << "The memory address held by stringREF      :[";
+	std::cout << &stringREF << "]." << std::endl;
+
+	std::cout << "--------------------------------------------------------------" << std::endl;
+
+	std::cout << "The value of the string variable          :{ ";
+	std::cout << StrTest + " }." << std::endl;
+
+	std::cout << "The value pointed to by stringPTR         :{ ";
+	std::cout << *stringPTR + " }." << std::endl;
+
+	std::cout << "The value of the string variable          :{ ";
+	std::cout << stringREF << " }." << std::endl;
+
+	return EXIT_SUCCESS;
+}
