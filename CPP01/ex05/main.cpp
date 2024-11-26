541bc7da00bb8f41f19048ab9b2524395c74b878
ychagri <ychagri@student.1337.ma>
2024-11-26 16:47:02 +0100

    ex05 donw

---
+++ CPP01/ex05/main.cpp
@@ -0,0 +1,14 @@
+# include "Harl.hpp"
+
+int	main()
+{
+	Harl	myObj;
+
+	myObj.complain("DEBUG");
+	myObj.complain("INFO");
+	myObj.complain("WARNING");
+	myObj.complain("ERROR");
+	myObj.complain("ELSE");
+	
+	return 0;
+}
