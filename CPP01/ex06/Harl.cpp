0d34e21b4d1747cf3e503cefcd27a9da471ecb85
ychagri <ychagri@student.1337.ma>
2024-11-26 20:15:33 +0100

    ex06 05 done

--- CPP01/ex06/Harl.cpp
+++ CPP01/ex06/Harl.cpp
@@ -12,21 +12,21 @@ Harl::~Harl( void )
 
 void	Harl::debug( void )
 {
-	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. ";
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger." << std::endl;
 	std::cout << "I really do!";
 	std::cout << std::endl;
 }
 
 void	Harl::info( void )
 {
-	std::cout << "I cannot believe adding extra bacon costs more money.";
+	std::cout << "I cannot believe adding extra bacon costs more money." << std::endl;
 	std::cout << " You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!";
 	std::cout << std::endl;
 }
 
 void	Harl::warning( void )
 {
-	std::cout << "I think I deserve to have some extra bacon for free.";
+	std::cout << "I think I deserve to have some extra bacon for free." << std::endl;
 	std::cout << "I\'ve been coming for years whereas you started working here since last month.";
 	std::cout << std::endl;
 }
