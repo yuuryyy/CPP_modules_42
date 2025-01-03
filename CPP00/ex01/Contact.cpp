7b23f75881d8737eb067e5296a4008fd554f4a49
ychagri <ychagri@student.1337.ma>
2025-01-03 05:27:03 +0100

    cpp00ex01 fixes

--- CPP00/ex01/Contact.cpp
+++ CPP00/ex01/Contact.cpp
@@ -3,36 +3,36 @@
 
 Contact::Contact()
 {
-	this->_FirstName = "";
-	this->_LastName = "";
-	this->_Nickname = "";
-	this->_PhoneNumber = "";
-	this->_DarkestSecret = "";
+	_FirstName = "";
+	_LastName = "";
+	_Nickname = "";
+	_PhoneNumber = "";
+	_DarkestSecret = "";
 }
 
 
 Contact::~Contact()
 {
-	this->_FirstName = "";
-	this->_LastName = "";
-	this->_Nickname = "";
-	this->_PhoneNumber = "";
-	this->_DarkestSecret = "";
+	_FirstName = "";
+	_LastName = "";
+	_Nickname = "";
+	_PhoneNumber = "";
+	_DarkestSecret = "";
 }
 
 
 void	Contact::SetContactField(const std::string& ContactInfo, int flag)
 {
 	if (flag == FNAME)
-		this->_FirstName = ContactInfo;
+		_FirstName = ContactInfo;
 	else if (flag == LNAME)
-		this->_LastName = ContactInfo;
+		_LastName = ContactInfo;
 	else if (flag == NNAME)
-		this->_Nickname = ContactInfo;
+		_Nickname = ContactInfo;
 	else if (flag == NUM)
-		this->_PhoneNumber = ContactInfo;
+		_PhoneNumber = ContactInfo;
 	else if (flag == SECRET)
-		this->_DarkestSecret = ContactInfo;
+		_DarkestSecret = ContactInfo;
 }
 
 const std::string	Contact::fitSearchDisplay(const std::string& Info)
@@ -53,15 +53,15 @@ const std::string	Contact::fitSearchDisplay(const std::string& Info)
 
 void	Contact::DisplaySearch(const int index)
 {
-	std::cout << "      |    " << index << "     |" << fitSearchDisplay(this->_FirstName)
-	<< fitSearchDisplay(this->_LastName) << fitSearchDisplay(this->_Nickname) << std::endl;
+	std::cout << "      |    " << index << "     |" << fitSearchDisplay(_FirstName)
+	<< fitSearchDisplay(_LastName) << fitSearchDisplay(_Nickname) << std::endl;
 }
 
 void	Contact::DisplayInfo()
 {
-	std::cout << MAGENTA "\nFirst name 	 : " RESET << this->_FirstName << std::endl;
-	std::cout << MAGENTA "Last name  	 : " RESET << this->_LastName << std::endl;
-	std::cout << MAGENTA "Nickname   	 : " RESET << this->_Nickname << std::endl;
-	std::cout << MAGENTA "Phone number     : " RESET << this->_PhoneNumber << std::endl;
-	std::cout << MAGENTA "Darkest secret   : " RESET << this->_DarkestSecret + "\n" << std::endl;
+	std::cout << MAGENTA "\nFirst name 	 : " RESET <<_FirstName << std::endl;
+	std::cout << MAGENTA "Last name  	 : " RESET << _LastName << std::endl;
+	std::cout << MAGENTA "Nickname   	 : " RESET << _Nickname << std::endl;
+	std::cout << MAGENTA "Phone number     : " RESET << _PhoneNumber << std::endl;
+	std::cout << MAGENTA "Darkest secret   : " RESET << _DarkestSecret + "\n" << std::endl;
 }
