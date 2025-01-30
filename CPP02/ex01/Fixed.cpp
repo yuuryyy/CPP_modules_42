4729a082bb024d6bff4d1d11ed0b101e18402681
Youssra Chagri <ychagri@e1r2p3.1337.ma>
2025-01-30 21:21:14 +0100

    cpp02 post change

---
+++ CPP02/ex01/Fixed.cpp
@@ -0,0 +1,74 @@
+#include "Fixed.hpp"
+
+const int Fixed::fractionalBITS = 8;
+
+Fixed::Fixed( void ) : _value(0)
+{
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed( const Fixed &Obj)
+{
+    std::cout << "Copy constructor called" << std::endl;
+    _value = Obj.getRawBits();
+}
+
+
+Fixed &Fixed::operator=( const Fixed &Obj )
+{
+    std::cout << "Copy assignment operator called" << std::endl;
+    if (this != &Obj)
+    {
+        _value = Obj.getRawBits();
+    }
+    return *this;
+}
+
+Fixed::Fixed( const int Number)
+{
+    std::cout << "Int constructor called" << std::endl;
+    _value = Number * (1 << fractionalBITS);
+}
+
+Fixed::Fixed( const float Number)
+{
+    float   result;
+
+    std::cout << "Float constructor called" << std::endl;
+    result = Number * (1 << fractionalBITS);
+    std::cout << result << std::endl;
+    _value = roundf(result);
+    std::cout << _value << std::endl;
+// 
+}
+
+Fixed::~Fixed( void )
+{
+    std::cout << "Destructor called" << std::endl;
+    _value = 0;
+}
+
+int Fixed::getRawBits( void ) const
+{
+    std::cout << "getRawBits member function called" << std::endl;
+    return _value;
+}
+
+void Fixed::setRawBits( int const raw )
+{
+    std::cout << "setRawBits member function called " << std::endl;
+    _value = raw;
+}
+
+float   Fixed::toFloat( void ) const
+{
+    float   result = _value / (1 << fractionalBITS);
+    std::cout << result << std::endl;
+
+    return result;
+}
+
+int     Fixed::toInt( void ) const
+{
+    return _value / (1 << fractionalBITS);
+}
---
+++ CPP02/ex01/Fixed.hpp
@@ -0,0 +1,32 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+    private:
+       int              _value;
+       static const int fractionalBITS;
+       
+    public:
+
+        Fixed( void );
+        Fixed( const Fixed &Obj );
+        Fixed &operator=( const Fixed &Obj );
+        ~Fixed( void );
+
+        Fixed( const int Number );
+        Fixed( const float Number );
+
+        int getRawBits( void ) const;
+        void setRawBits( int const raw );
+
+        float toFloat( void ) const;
+        int   toInt( void ) const;
+
+};
+
+
+#endif
