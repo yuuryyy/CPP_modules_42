112a77fdab98633b2e9ffa82461c24beea1d2a6c
Youssra Chagri <ychagri@e1r8p14.1337.ma>
2025-01-31 03:01:24 +0100

    cpp02 ex02 entry

---
+++ CPP02/ex02/Fixed.hpp
@@ -0,0 +1,40 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+    private:
+       int            _value;
+       static const int fractionalBITS;
+       
+    public:
+
+        Fixed( void );
+        Fixed( const Fixed &Obj );
+        Fixed &operator=( const Fixed &Obj );
+        ~Fixed( void );
+
+        bool operator>(const Fixed &other);
+        bool operator<(const Fixed &other);
+        bool operator>=(const Fixed &other);
+        bool operator<=(const Fixed &other);
+        bool operator==(const Fixed &other);
+        bool operator!=(const Fixed &other);
+
+        Fixed( const int Number );
+        Fixed( const float Number );
+
+        int getRawBits( void ) const;
+        void setRawBits( int const raw );
+
+        float toFloat( void ) const;
+        int   toInt( void ) const;
+
+};
+
+std::ostream &operator<<(std::ostream &__out, const Fixed &obj);
+
+#endif
