430410c05d7fcda617d7bfcfb8ab3c0dbc49af19
Youssra Chagri <ychagri@e1r4p4.1337.ma>
2025-02-14 22:36:48 +0100

    ex02 cpp03

--- CPP03/ex02/main.cpp
+++ CPP03/ex02/main.cpp
@@ -1,13 +1,21 @@
+#include "FragTrap.hpp"
 #include "ScavTrap.hpp"
 
 int main( void )
 {
-    ScavTrap Scavtrap("booo");
+    FragTrap fragTrap("FragTrap");
+    FragTrap fragTrap2(fragTrap);
+    FragTrap fragTrap3 = fragTrap;
 
-    Scavtrap.attack("fooo");
-    Scavtrap.takeDamage(5);
-    Scavtrap.takeDamage(5);
-    Scavtrap.takeDamage(5);
-    Scavtrap.beRepaired(5);
+    fragTrap.attack("target");
+    fragTrap.takeDamage(10);
+    fragTrap.beRepaired(5);
+    fragTrap.highFiveGuys();
+
+    fragTrap2.attack("target");
+    fragTrap2.takeDamage(10);
+
+    fragTrap3.attack("target");
+    fragTrap3.takeDamage(10);
     return 0;
 }
