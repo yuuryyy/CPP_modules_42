27094b200e94e6e8ccbb5f1458560ea5d4efa829
yuuryyy <youssrachagri@gmail.com>
2025-07-11 01:43:37 +0100

    cpp08 ex00 first run working on basic tests

--- CPP08/ex00/easyfind.hpp
+++ CPP08/ex00/easyfind.hpp
@@ -6,7 +6,27 @@
 /*   By: ychagri <ychagri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 01:13:02 by ychagri           #+#    #+#             */
-/*   Updated: 2025/07/03 01:13:03 by ychagri          ###   ########.fr       */
+/*   Updated: 2025/07/11 01:37:55 by ychagri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef EASY_FIND__
+#define EASY_FIND__
+
+#include <algorithm>
+#include <iterator>
+#include <vector>
+#include <iostream>
+
+template<typename T>
+typename T::iterator    easyfind(T &countainer, const int val)
+{
+    typename T::iterator it = countainer.begin();
+    
+    for (; it != countainer.end(); it++)
+        if (*it == val)
+            break;
+    return it;
+}
+
+#endif
--- CPP08/ex00/main.cpp
+++ CPP08/ex00/main.cpp
@@ -6,7 +6,32 @@
 /*   By: ychagri <ychagri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/03 01:01:15 by ychagri           #+#    #+#             */
-/*   Updated: 2025/07/03 01:12:48 by ychagri          ###   ########.fr       */
+/*   Updated: 2025/07/11 01:42:42 by ychagri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "easyfind.hpp"
+
+int main()
+{
+    std::vector<int> vec;
+
+    vec.push_back(1);
+    vec.push_back(12);
+    vec.push_back(71);
+    vec.push_back(19);
+    vec.push_back(100);
+    vec.push_back(761);
+
+    int i = 14;
+    std::vector<int>::iterator it =  easyfind(vec, i);
+
+    if ( it == vec.end())
+        std::cout << i << " Was not Found !!";
+    else
+        std::cout << *it << " was Found";
+    
+    std::cout << std::endl;
+    
+    return 0;
+}
